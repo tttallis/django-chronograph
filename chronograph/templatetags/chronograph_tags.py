a0a3cda57be4fbc81d2439bd2bd2d1e5dac89a8f
JJ Geewax <jj@invitemedia.com>
2010-03-14 12:16:35 -0400

    Removed debug print statement and added new template tag for getting the URL to view logs for a specified job

--- chronograph/templatetags/chronograph_tags.py
+++ chronograph/templatetags/chronograph_tags.py
@@ -9,7 +9,6 @@ class RunJobURLNode(template.Node):
         
     def render(self, context):
         object_id = self.object_id.resolve(context)
-        print object_id
         try:
             # Old way
             url = reverse('chronograph_job_run', args=(object_id,))
@@ -33,4 +32,37 @@ def do_get_run_job_url(parser, token):
         raise template.TemplateSyntaxError, "%r tag requires one argument" % token.contents.split()[0]
     return RunJobURLNode(object_id)
 
+class ViewLogsURLNode(template.Node):
+    def __init__(self, object_id):
+        self.object_id = template.Variable(object_id)
+        
+    def render(self, context):
+        object_id = self.object_id.resolve(context)
+        try:
+            # Old way
+            url = reverse('chronograph_log_changelist')
+        except NoReverseMatch:
+            # New way
+            url = reverse('admin:chronograph_log_changelist')
+        
+        url += '?job=%s' % object_id
+        
+        return url
+
+def do_get_job_view_logs_url(parser, token):
+    """
+    Returns the URL to the view that does the 'view_logs' command.
+    
+    Usage::
+    
+        {% get_job_view_logs_url [object_id] %}
+    """
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, object_id = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires one argument" % token.contents.split()[0]
+    return ViewLogsURLNode(object_id)
+
 register.tag('get_run_job_url', do_get_run_job_url)
+register.tag('get_job_view_logs_url', do_get_job_view_logs_url)
